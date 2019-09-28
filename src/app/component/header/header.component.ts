ec7d4e97f2d941f7d19b0f1af5ce366adc189307
l08084 <l08084.1989@gmail.com>
2019-09-28 22:26:11 +0900

    add signOut

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { SpinnerService } from 'src/app/services/spinner.service';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-header',
@@ -8,7 +11,31 @@ import { Component, OnInit, Input } from '@angular/core';
 export class HeaderComponent implements OnInit {
   @Input() isSigningIn: boolean;
 
-  constructor() {}
+  constructor(
+    private afAuth: AngularFireAuth,
+    private router: Router,
+    private spinnerService: SpinnerService
+  ) {}
 
   ngOnInit() {}
+
+  /**
+   * ログアウト処理
+   *
+   * @memberof HeaderComponent
+   */
+  public signOut(): void {
+    // スピナー表示
+    this.spinnerService.show();
+    // ログアウトAPIを呼び出す
+    this.afAuth.auth
+      .signOut()
+      .then(() => {
+        // ログアウトが成功したら、ログイン画面に遷移
+        this.router.navigate(['/login']);
+      })
+      .catch(error => console.log(error))
+      // 一連の処理が完了したらスピナーを消す
+      .finally(() => this.spinnerService.hide());
+  }
 }
