b9c8ca865c5fb44dd939a3ba26f46972e004db97
l08084 <l08084.1989@gmail.com>
2019-10-14 19:33:56 +0900

    update

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-create',
@@ -6,10 +12,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create.component.scss']
 })
 export class CreateComponent implements OnInit {
+  // FormGroup定義
+  public createFormGroup: FormGroup;
+  // Titleフォームのコントロール定義
+  public titleControl: FormControl;
+  // descriptionフォームのコントロール定義
+  public descriptionControl: FormControl;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+    this.titleControl = this.createFormGroup.get('title') as FormControl;
+    this.descriptionControl = this.createFormGroup.get(
+      'description'
+    ) as FormControl;
   }
 
+  ngOnInit() {}
+
+  /**
+   * フォーム設定の作成
+   *
+   */
+  private createForm() {
+    this.createFormGroup = this.fb.group({
+      title: ['', [Validators.required]],
+      description: ['', [Validators.required]]
+    });
+  }
+
+  onSubmit() {
+    console.log(`${this.titleControl.value}/${this.descriptionControl.value}`);
+  }
 }
