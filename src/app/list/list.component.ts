dac269edeb76c0afebb7485f1b3b4bb57c2fa230
l08084 <l08084.1989@gmail.com>
2019-11-04 16:51:19 +0900

    メモの一覧表示

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Memo } from '../entity/memo.entity';
+import {
+  AngularFirestoreCollection,
+  AngularFirestore
+} from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-list',
@@ -6,10 +11,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
+  public memo: Memo;
+  public memos: Memo[];
+  public memoCollection: AngularFirestoreCollection<Memo>;
 
-  constructor() { }
+  constructor(private afStore: AngularFirestore) {}
 
   ngOnInit() {
+    this.retrieveMemos();
   }
 
+  public retrieveMemos() {
+    this.memoCollection = this.afStore.collection('memos', ref =>
+      ref.orderBy('createdDate', 'desc')
+    );
+
+    this.memoCollection.valueChanges().subscribe(data => {
+      this.memos = data;
+      console.log(this.memos);
+    });
+  }
 }
