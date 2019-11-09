bbc44cb9f830f2341ab672a41c12ccded0f20362
l08084 <l08084.1989@gmail.com>
2019-11-09 21:42:09 +0900

    update

--- src/app/update/update.component.ts
+++ src/app/update/update.component.ts
@@ -1,5 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators
+} from '@angular/forms';
+import { Memo } from '../entity/memo.entity';
+import { AngularFireAuth } from '@angular/fire/auth';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection
+} from '@angular/fire/firestore';
+import { SpinnerService } from '../services/spinner.service';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-update',
@@ -7,12 +21,88 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
   styleUrls: ['./update.component.scss']
 })
 export class UpdateComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {}
+  // FormGroup定義
+  public createFormGroup: FormGroup;
+  // Titleフォームのコントロール定義
+  public titleControl: FormControl;
+  // descriptionフォームのコントロール定義
+  public descriptionControl: FormControl;
+  public memo: Memo;
+  public memoCollection: AngularFirestoreCollection<Memo>;
+  constructor(
+    private route: ActivatedRoute,
+    private fb: FormBuilder,
+    private afAuth: AngularFireAuth,
+    private afStore: AngularFirestore,
+    private spinnerService: SpinnerService
+  ) {
+    this.createForm();
+    this.titleControl = this.createFormGroup.get('title') as FormControl;
+    this.descriptionControl = this.createFormGroup.get(
+      'description'
+    ) as FormControl;
+  }
 
   ngOnInit() {
     this.route.paramMap.subscribe((params: ParamMap) => {
       const param = params.get('memo');
       console.log(param);
     });
+
+    this.retrieveMemos();
+  }
+
+  /**
+   * フォーム設定の作成
+   *
+   */
+  private createForm() {
+    this.createFormGroup = this.fb.group({
+      title: ['', [Validators.required]],
+      description: ['', [Validators.required]]
+    });
+  }
+
+  onSubmit() {
+    this.spinnerService.show();
+    console.log(`${this.titleControl.value}/${this.descriptionControl.value}`);
+
+    const user = this.afAuth.auth.currentUser;
+
+    console.log(` uid: ${user.uid} \n providerId: ${user.providerId}`);
+    console.log(` email: ${user.email}\n photoURL: ${user.photoURL}`);
+    console.log(
+      ` providerData: ${user.providerData}\n emailVerified: ${
+        user.emailVerified
+      }`
+    );
+    console.log(
+      ` displayName: ${user.displayName}\n phoneNumber: ${user.phoneNumber}`
+    );
+    this.memo = {
+      id: '',
+      title: this.titleControl.value,
+      description: this.descriptionControl.value,
+      createdUser: user.uid,
+      createdDate: firebase.firestore.FieldValue.serverTimestamp()
+    };
+    this.afStore
+      .collection('memos')
+      .add(this.memo)
+      .then(docRef => {
+        this.memoCollection.doc(docRef.id).update({
+          id: docRef.id
+        });
+        this.createFormGroup.reset();
+        this.spinnerService.hide();
+      });
+    console.log('memo');
+    console.log(this.memo);
+  }
+
+  public retrieveMemos() {
+    this.memoCollection = this.afStore.collection('memos', ref =>
+      ref.orderBy('createdDate', 'desc')
+    );
   }
 }
