61f90fc6a4ba26de16e6d0cf77f4f907a4ecbecd
l08084 <l08084.1989@gmail.com>
2019-12-13 20:30:40 +0900

    update

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SpinnerService } from 'src/app/services/spinner.service';
 import { Router } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/auth';
