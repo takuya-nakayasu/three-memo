2cfa7dc3db627905f569d18f6251160738f9c2f5
l08084 <l08084.1989@gmail.com>
2019-12-13 19:15:37 +0900

    update

--- src/app/services/toast.service.ts
+++ src/app/services/toast.service.ts
@@ -5,7 +5,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
  * トーストの表示を制御するサービス
  *
  * @export
- * @class SpinnerService
+ * @class ToastService
  */
 @Injectable({
   providedIn: 'root'
@@ -17,7 +17,14 @@ export class ToastService {
   // tslint:disable-next-line:variable-name
   constructor(private _snackBar: MatSnackBar) {}
 
+  /**
+   * トーストを表示する
+   *
+   * @param {string} message トーストに表示するメッセージ
+   * @memberof ToastService
+   */
   public open(message: string): void {
+    // 2000ミリ秒間トーストを表示する
     this._snackBar.open(message, this._actionMessage, {
       duration: 2000
     });
