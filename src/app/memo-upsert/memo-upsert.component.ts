bbfb77dc3e6390272473a598557e6e4d30ba37a8
l08084 <l08084.1989@gmail.com>
2020-03-01 21:02:20 +0900

    refactoring

--- src/app/memo-upsert/memo-upsert.component.ts
+++ src/app/memo-upsert/memo-upsert.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-memo-upsert',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./memo-upsert.component.scss']
 })
 export class MemoUpsertComponent implements OnInit {
-
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      const memoId = params.get('id');
+      const folderId: string = params.get('folderId');
+      console.log(`upsert-memoId: ${memoId}`);
+      console.log(`upsert-folderId: ${folderId}`);
+    });
   }
-
 }
