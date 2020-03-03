ace87c2a38a791db5fab90a1c2b44d035cfc1b20
l08084 <l08084.1989@gmail.com>
2020-03-03 19:20:49 +0900

    add upsert routing param entity

---
+++ src/app/entity/upsert-routing-param.entity.ts
@@ -0,0 +1,10 @@
+/**
+ * メモの新規作成と更新画面に遷移する時に渡すパラメーターオブジェクト
+ *
+ * @export
+ * @interface UpsertRoutingParam
+ */
+export interface UpsertRoutingParam {
+  selectedMemoId: string;
+  selectedFolderId: string;
+}
