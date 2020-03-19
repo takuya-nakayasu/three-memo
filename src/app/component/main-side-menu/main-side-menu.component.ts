81356815234ddf774d15a7f31bb9096913f10086
l08084 <l08084.1989@gmail.com>
2020-03-19 21:04:49 +0900

    click menu drawer close

--- src/app/component/main-side-menu/main-side-menu.component.ts
+++ src/app/component/main-side-menu/main-side-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { UpsertRoutingParam } from '../../entity/upsert-routing-param.entity';
 
@@ -16,6 +16,9 @@ import { UpsertRoutingParam } from '../../entity/upsert-routing-param.entity';
   styleUrls: ['./main-side-menu.component.scss']
 })
 export class MainSideMenuComponent implements OnInit {
+  @Input() isHandset: boolean;
+  @Output() drawerClosed = new EventEmitter<void>();
+
   constructor(private router: Router) {}
 
   ngOnInit() {}
@@ -26,11 +29,18 @@ export class MainSideMenuComponent implements OnInit {
    * @memberof MainSideMenuComponent
    */
   public gotoUpsert(isCreate = false) {
+    this.drawerClose();
     const param: UpsertRoutingParam = {
       selectedMemoId: '',
       selectedFolderId: '',
       isCreate
     };
     this.router.navigate(['/home/upsert', param]);
+  }
+
+  public drawerClose() {
+    if (this.isHandset) {
+      this.drawerClosed.emit();
+    }
   }
 }
